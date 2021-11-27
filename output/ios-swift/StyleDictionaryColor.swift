45b2ffef39bdf59f4fe44e09e31cf51cc896811a
jaylpl <jaylpl@users.noreply.github.com>
2021-11-27 16:36:42 +0000

    Update platform deliverables

--- output/ios-swift/StyleDictionaryColor.swift
+++ output/ios-swift/StyleDictionaryColor.swift
@@ -4,33 +4,17 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 24 Nov 2021 19:44:35 GMT
+// Generated on Sat, 27 Nov 2021 16:36:41 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryColor {
-    public static let backgroundButtonPrimary = UIColor(red: 0.184, green: 0.502, blue: 0.929, alpha: 1)
-    public static let backgroundInputDefault = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
-    public static let backgroundInputInvalid = UIColor(red: 0.200, green: 0.200, blue: 0.200, alpha: 1)
-    public static let black = UIColor(red: 0.200, green: 0.200, blue: 0.200, alpha: 1)
-    public static let blue1 = UIColor(red: 0.184, green: 0.502, blue: 0.929, alpha: 1)
-    public static let blue2 = UIColor(red: 0.176, green: 0.612, blue: 0.859, alpha: 0.9019607843137255)
-    public static let blue3 = UIColor(red: 0.337, green: 0.800, blue: 0.949, alpha: 0.8)
-    public static let blue4 = UIColor(red: 0.337, green: 0.800, blue: 0.949, alpha: 0.32941176470588235)
-    public static let borderButtonPrimary = UIColor(red: 0.176, green: 0.612, blue: 0.859, alpha: 1)
-    public static let borderInputNormal = UIColor(red: 0.200, green: 0.200, blue: 0.200, alpha: 1)
-    public static let borderSelectFocus = UIColor(red: 0.176, green: 0.612, blue: 0.859, alpha: 1)
-    public static let gray1 = UIColor(red: 0.310, green: 0.310, blue: 0.310, alpha: 1)
-    public static let gray2 = UIColor(red: 0.510, green: 0.510, blue: 0.510, alpha: 1)
-    public static let gray3 = UIColor(red: 0.741, green: 0.741, blue: 0.741, alpha: 1)
-    public static let gray4 = UIColor(red: 0.878, green: 0.878, blue: 0.878, alpha: 1)
-    public static let gray5 = UIColor(red: 0.949, green: 0.949, blue: 0.949, alpha: 1)
-    public static let greenDark = UIColor(red: 0.129, green: 0.588, blue: 0.325, alpha: 1)
-    public static let greenLight = UIColor(red: 0.435, green: 0.812, blue: 0.592, alpha: 1)
-    public static let greenMid = UIColor(red: 0.153, green: 0.682, blue: 0.376, alpha: 1)
-    public static let orange = UIColor(red: 0.949, green: 0.600, blue: 0.290, alpha: 1)
-    public static let red = UIColor(red: 0.922, green: 0.341, blue: 0.341, alpha: 1)
-    public static let white = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
-    public static let yellow = UIColor(red: 0.949, green: 0.788, blue: 0.298, alpha: 1)
+    public static let colorsMultipleFills0 = UIColor(red: 0.251, green: 1.000, blue: 0.729, alpha: 1)
+    public static let colorsMultipleFills1 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.1)
+    public static let colorsSingleBlue = UIColor(red: 0.016, green: 0.290, blue: 1.000, alpha: 1)
+    public static let colorsSpecialCharacters = UIColor(red: 0.251, green: 0.875, blue: 0.314, alpha: 1) /* Emoji */
+    public static let colorsSpecialCharactersNderung = UIColor(red: 0.204, green: 0.337, blue: 0.686, alpha: 1)
+    public static let darkBackground = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let lightBackground = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
 }
--- output/ios/StyleDictionaryColor.h
+++ output/ios/StyleDictionaryColor.h
@@ -4,35 +4,19 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 24 Nov 2021 19:44:35 GMT
+// Generated on Sat, 27 Nov 2021 16:36:41 GMT
 
 
 #import <UIKit/UIKit.h>
 
 typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
-ColorBlack,
-ColorWhite,
-ColorGray1,
-ColorGray2,
-ColorGray3,
-ColorGray4,
-ColorGray5,
-ColorRed,
-ColorOrange,
-ColorYellow,
-ColorBlue1,
-ColorBlue2,
-ColorBlue3,
-ColorGreenDark,
-ColorGreenMid,
-ColorGreenLight,
-ColorBlue4,
-ColorBorderButtonPrimary,
-ColorBorderSelectFocus,
-ColorBorderInputNormal,
-ColorBackgroundButtonPrimary,
-ColorBackgroundInputInvalid,
-ColorBackgroundInputDefault
+ColorColorsMultipleFills0,
+ColorColorsMultipleFills1,
+ColorColorsSingleBlue,
+ColorColorsSpecialCharacters,
+ColorColorsSpecialCharactersNderung,
+ColorLightBackground,
+ColorDarkBackground
 };
 
 @interface StyleDictionaryColor : NSObject
